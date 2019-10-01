18ae770b05fd9f203b34f7e5915b606eb516f627
Desarrollo <damoya86@gmail.com>
2019-10-01 17:06:18 -0500

    2019-10-01

--- src/app/contents/contents.component.ts
+++ src/app/contents/contents.component.ts
@@ -43,25 +43,5 @@ export class ContentsComponent implements OnInit {
         console.error(error);
       }
     );
-    //Naves
-    this.dataSWService.getNavs()
-    .subscribe(
-      (data) => { //Sucess
-        this.navsNombre = data['name'];
-      },
-      (error) => {
-        console.error(error);
-      } 
-    );
-
-    this.dataSWService.getNavs()
-    .subscribe(
-      (data) => { //Sucess
-        this.navs = data['results'];
-      },
-      (error) => {
-        console.error(error);
-      } 
-    );
   }
 }
